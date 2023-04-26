98582f30840724acee1d2e67f8cfac5aa5193701
Stephen <stephen_1996@live.ca>
2023-04-26 12:15:00 -0700

    opening chat gpt in script

--- ChatTesting.py
+++ ChatTesting.py
@@ -1,3 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import keys
 
+# Starts a webdriver isntance and opens chatGPT
+driver = webdrive.Chrome()
+driver.get('https://chatgpt.openai.com/')
+
