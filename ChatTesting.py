2db41ba621801fed9b304bd55c2db7b5266150a2
Stephen <stephen_1996@live.ca>
2023-04-26 12:23:41 -0700

    finding input id to write to chatbot

--- ChatTesting.py
+++ ChatTesting.py
@@ -3,5 +3,8 @@ from selenium.webdriver.common.keys import keys
 
 # Starts a webdriver isntance and opens chatGPT
 driver = webdrive.Chrome()
-driver.get('https://chatgpt.openai.com/')
+driver.get('https://chat.openai.com/')
 
+# Finds input field and writes question and send
+input_field = driver.find_element_by_class_name('c-text-input')
+input_field.send_keys('')
