64425e36003b4caf6b2aa87015c1f15841db3107
Stephen <stephen_1996@live.ca>
2023-04-26 20:23:28 -0700

    got basic scripting to work

--- ChatTesting.py
+++ ChatTesting.py
@@ -3,8 +3,15 @@ from selenium.webdriver.common.keys import Keys
 
 # Starts a webdriver isntance and opens chatGPT
 driver = webdriver.Chrome(executable_path='/Users/stephenhuang/Desktop/Script Testing/chromedriver_mac64/chromedriver')
-driver.get('https://google.com/')
+driver.get('https://www.google.com/')
 
 # Finds input field and writes question and send
 input_field = driver.find_element_by_class_name('gLFyf')
-input_field.send_keys('hi')
+input_field.send_keys('nba')
+input_field.send_keys(Keys.RETURN)
+
+# Wait
+driver.implicity_wait(10)
+
+# Get Results
+
